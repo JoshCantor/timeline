56faa168dd583da839688bc84ffe7bf38f1a4c46
Josh Cantor <jcantor@nextdoor.com>
2019-04-01 00:56:27 -0700

    Reworking getCalendar

--- src/utils.js
+++ src/utils.js
@@ -21,9 +21,9 @@ export const getTimeDifferenceInDays = (startDate, endDate) => {
   return Math.ceil(timeDistance / (1000 * 60 * 60 * 24));
 };
 
-const addToCalendarIfNotPresent = (calendar, date) => {
-  const year = date.getFullYear();
-  const monthZeroIndex = date.getMonth();
+const addToCalendarIfNotPresent = (calendar, year, monthZeroIndexed) => {
+  // const year = date.getFullYear();
+  // const monthZeroIndex = date.getMonth();
   const month = monthZeroIndex + 1;
   const daysInMonth = new Date(year, month, 0).getDate();
 
@@ -35,13 +35,57 @@ const addToCalendarIfNotPresent = (calendar, date) => {
   }
 };
 
-export const getCalendar = (items) => {
-  const calendar = {};
+const getLatestEndDate = (items) => {
+  let latestEndDate = null;
   items.forEach((item) => {
-    const startDate = getTimeZoneAwareDateFromStr(item.start);
-    const endDate = getTimeZoneAwareDateFromStr(item.end);
-    addToCalendarIfNotPresent(calendar, startDate);
-    addToCalendarIfNotPresent(calendar, endDate);
+    const currentEndDate = new Date(item.end);
+    if (!latestEndDate || currentEndDate > latestEndDate) {
+      latestEndDate = currentEndDate;
+    }
   });
+  return latestEndDate;
+};
+
+export const getCalendar = (sortedItems) => {
+  const itemWithEarliestStart = sortedItems[0];
+  const earliestStartDate = getTimeZoneAwareDateFromStr(itemWithEarliestStart.start);
+  const latestEndDate = getLatestEndDate(sortedItems);
+  const earliestStartDateYear = earliestStartDate.getFullYear();
+  const latestEndDateYear = latestEndDate.getFullYear();
+  const earliestStartDateMonth = earliestStartDate.getMonth();
+  const latestEndDateMonth = latestEndDate.getMonth();
+
+  const calendar = {};
+
+  let year = earliestStartDateYear;
+  while (year <= latestEndDateYear) {
+    const month = 0;
+    const december = 11;
+
+    if (year === earliestStartDateYear) {
+      let month = earliestStartDateMonth;
+      while (month <= december) {
+        addToCalendarIfNotPresent(calendar, year, month);
+        month ++
+      }
+    } else if (year === latestEndDateYear) {
+      let mon
+    }
+
+    year++
+  }
+  // items.forEach((item) => {
+  //   const startDate = getTimeZoneAwareDateFromStr(item.start);
+  //   const endDate = getTimeZoneAwareDateFromStr(item.end);
+  //   const startMonth = startDate.getMonth();
+  //   const endMonth = startDate.getMonth();
+  //
+  //   //
+  //   if (endMonth - startMonth <= 1) {
+  //     addToCalendarIfNotPresent(calendar, startDate);
+  //     addToCalendarIfNotPresent(calendar, endDate);
+  //   }
+  //
+  // });
   return calendar;
 };
